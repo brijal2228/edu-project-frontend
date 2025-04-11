81649c3a3fd1723e19ac0eda80fa5141012a5133
hena-patel <hennypatel78@gamil.com>
2025-04-11 15:49:02 +0530

    clear comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import './App.css';
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Navbar from "./components/navbar";
-// import Home from "./components/home";
-// import Login from "./components/login";
-// import Registration from "./components/register";
-// import Books from "./components/books";
-// import Notes from "./components/notes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import Login from "./components/login";
+import Registration from "./components/register";
+import Books from "./components/books";
+import Notes from "./components/notes";
 import UserProfile from "./components/userprofile";
 // import Profile from "./components/userprofile";
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/login", element: <Login /> },
-//   {path:"/notes",element:<Notes/>},
-//   { path: "/register", element: <Registration/> },
-//   { path: "/books", element: <Books /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  {path:"/notes",element:<Notes/>},
+  { path: "/register", element: <Registration/> },
+  { path: "/books", element: <Books /> },
+]);
 
 const App: React.FC = () => {
   return (
     <>
      <UserProfile/> 
-      {/* <Navbar /> 
+      <Navbar /> 
       <div > 
         <RouterProvider router={router} />
       
-      </div>  */}
+      </div> 
     </>
   );
 };
